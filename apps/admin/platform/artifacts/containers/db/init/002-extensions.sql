83912834da8a74d62e3a459742b5ca9c23f88a5f
Angel Szymczak <contact@angelpixel.io>
2026-04-26 17:50:53 -0300

    chore(platform): add hybrid local infra layout for admin

---
+++ apps/admin/platform/artifacts/containers/db/init/002-extensions.sql
@@ -0,0 +1,6 @@
+CREATE EXTENSION IF NOT EXISTS "citext";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+\connect customers_manager_system_test
+CREATE EXTENSION IF NOT EXISTS "citext";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
