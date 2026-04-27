27f1dad2bed393dd5bfc02fa6fb67790526380e8
Angel Szymczak <contact@angelpixel.io>
2026-04-27 01:21:47 -0300

    fix(lookbook): auto-load previews on app boot

--- apps/admin/config/initializers/lookbook.rb
+++ apps/admin/config/initializers/lookbook.rb
@@ -4,4 +4,8 @@ if defined?(Lookbook)
     config.page_paths = [Rails.root.join("docs/lookbook")]
     config.preview_paths = [Rails.root.join("app/components/previews")]
   end
+
+  Rails.application.config.to_prepare do
+    ViewComponent::Preview.load_previews if defined?(ViewComponent::Preview)
+  end
 end
